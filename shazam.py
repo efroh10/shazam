d44ded16c564b5fd299f8f62379d2f584dfddc4c
Elijah <efroh10@gmail.com>
2021-06-22 14:14:16 -0700

    introduced modularity

--- shazam.py
+++ shazam.py
@@ -1,26 +1,8 @@
-import requests
+from getResponse import getResponse
 
 search = input('What song would you like to search for?')
 
-url = "https://shazam.p.rapidapi.com/search"
-
-querystring = {
-    "term": search,  # no need to use curly braces or anything?
-    "locale": "en-US",
-    "offset": "0",
-    "limit": "5"
-}
-
-headers = {
-    'x-rapidapi-key': "4819b84ebdmshd3385dc9375d94cp104332jsnf63093179aec",
-    'x-rapidapi-host': "shazam.p.rapidapi.com"
-    }
-
-response = requests.request(
-    "GET", url, headers=headers, params=querystring
-)
-
-hitList = response.json()['tracks']['hits']
+hitList = getResponse('search', 'term', search)['tracks']['hits']
 
 counter = 1
 
@@ -38,18 +20,7 @@ suggest = int(input(
 
 key = hitList[suggest-1]['track']['key']
 
-url2 = "https://shazam.p.rapidapi.com/songs/list-recommendations"
-
-querystring2 = {
-    "key": key,  # no need to use curly braces or anything?
-    "locale": "en-US"
-}
-
-response2 = requests.request(
-    "GET", url2, headers=headers, params=querystring2
-)
-
-recommendations = response2.json()['tracks']
+recommendations = getResponse('songs/list-recommendations', 'key', key)['tracks']
 
 for x in recommendations:
     print('Check out ' + x['title'] + ' by ' + x['subtitle'])
